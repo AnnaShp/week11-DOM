2c1e83520f6e8e810f78d4182fe961ca0be4d530
AnnaShp <nyutka_slutskaya@mail.ru>
2023-05-19 15:09:40 +0300

    comments and changes

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
+//достаем цены из HTML
 const price1 = document.querySelector('.price1').textContent;
 const price2 = document.querySelector('.price2').textContent;
 const price3 = document.querySelector('.price3').textContent;
 const price4 = document.querySelector('.price4').textContent;
+/*console.log(price1);
+console.log(price2);
+console.log(price3);
+console.log(price4);*/
 const currency = " руб";
+
+//выводит значение строки из HTML, сейчас там пусто
 let totalPrice = document.getElementById('totalPrice');
 //console.log (totalPrice.textContent);
+
+//сумма стоимости товаров
 function sum() {
     return +price1 + +price2 + +price3 + +price4;
 }
 
+//функция складывает стоимость товаров + валюта и выводит на экран корзины
 function showPrice() {
-    return + sum();
+    return + sum() + currency;
 }
 //console.log (showPrice());
-totalPrice.textContent = showPrice() + currency;
+totalPrice.textContent = showPrice();
 
-let button = document.getElementById('sale');
+
+/*document.getElementById("sale").addEventListener('click', function showNewPrice() {
+     return sum() * 0,8 + currency;
+     totalPrice.textContent= (`Цена со скидкой + ${showNewPrice} + currency`);
+});*/
+
+/*let button = document.getElementById(#sale);
 button.onclick = function showNewPrice() {
-    return showPrice * 0, 8 + currency;
+    return showPrice * 0.8 + currency;
 }
-totalPrice.textContent = showNewPrice() + currency;
+change.totalPrice.textContent = showNewPrice() + currency;*/
 
 
 
-/*console.log(price1);
-console.log(price2);
-console.log(price3);
-console.log(price4);
-console.log(showPrice());*/
+
+
 
 
 
