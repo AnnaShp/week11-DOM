d9d6f552f7ccb52fcd509f4a1d84032cb4b56bdb
AnnaShp <nyutka_slutskaya@mail.ru>
2023-05-21 16:49:17 +0300

    second part hw

---
+++ star.js
@@ -0,0 +1,13 @@
+var number = Number(document.getElementById('input').value);
+
+let button = document.getElementById('check');
+button.onclick = function checkMe() {
+    return Number(number) ** 2;
+};
+
+let numberTwo = () => {
+    input.getAttribute('placeholder').value;
+    return input.setAttribute('checkMe()');
+};
+
+
